90b2ae0a4eb5093b065c546fd663d51cd3d7c4c6
Bhumika Vaishnani <bhumikavaishnani1990@gmail.com>
2023-11-02 10:32:28 -0400

    First commit

---
+++ models/contactModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const contactSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require: [true,"Please add contact name."]
+    },
+    email:{
+        type:String,
+        require: [true,"Please add contact email."]
+    },
+    phone:{
+        type:String,
+        require: [true,"Please add contact phone."]
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/dbConnection');
+const dotenv = require('dotenv').config;
+const app = express();
+
+connectDB();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(errorHandler);
+app.use("/api/contacts",require("./routes/contactRoutes"));
+
+app.listen(port, () =>{
+    console.log(`server running on port ${port}`);
+})
